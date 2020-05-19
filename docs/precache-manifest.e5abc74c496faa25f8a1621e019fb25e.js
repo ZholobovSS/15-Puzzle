45401e15224ebaa92af9913701e1978b39406c6b
ZholobovSS <zholobovsemyonsergeevich@gmail.com>
2020-05-20 00:29:59 +0400

    Add docs folder to project

---
+++ docs/precache-manifest.e5abc74c496faa25f8a1621e019fb25e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c3fdf1976b74eb4d1fe7c6ab3b83bb4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ee3051414cdf95f1cbf6",
+    "url": "./static/css/main.a8027f9e.chunk.css"
+  },
+  {
+    "revision": "e3518dd35e6ecddcc878",
+    "url": "./static/js/2.ffbb2f52.chunk.js"
+  },
+  {
+    "revision": "265068e7f3bc904803e029152d67e9a1",
+    "url": "./static/js/2.ffbb2f52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee3051414cdf95f1cbf6",
+    "url": "./static/js/main.b4cee49a.chunk.js"
+  },
+  {
+    "revision": "49316c510bc642edb3ac",
+    "url": "./static/js/runtime-main.a42b02ee.js"
+  }
+]);
